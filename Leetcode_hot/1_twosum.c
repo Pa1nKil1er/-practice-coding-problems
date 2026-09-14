433489fa5b7eaad8c8b4430fa91fae2f12948825
Pa1nKil1er <15105047833@163.com>
2026-09-14 22:51:07 +0800

    对leetcode的函数型提交不适应
    努力习惯

---
+++ Leetcode_hot/1_twosum.c
@@ -0,0 +1,19 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *ans=(int*)malloc(sizeof(int)*2);
+    //malloc返回值类型是void*
+
+    for(int i=0;i<numsSize;i++){
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[i]+nums[j]==target){
+                ans[0]=i;
+                ans[1]=j;
+                *returnSize = 2;  //必须要标明返回数组长度是2
+                return ans;
+            }
+        }
+    }
+    return ans;
+}
